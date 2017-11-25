221271050fea15747c8cfb39b168df582ab794a0
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2017-11-25 00:06:01 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,29 +1,43 @@
-class Maze:
-    def __init__(self):
-        self.color = "black"
-        self.height = 300
-        self.width = 300
+import pygame
+class Maze(pygame.sprite.Sprite):
+    def __init__(self, height, width, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.height = height
+        self.width = width
+        self.wall = pygame.Surface([width, height])
+        self.wall.fill = "blue"
+        self.rect = self.wall.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+   def setMaze()
+        walls = []   #list of tuples of walls in the form (height, width, x, y)
+        
 class Pacman:
-    def __init__(self):
-        self.color = 'yellow"
+    def __init__(self, pacImage):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(pacImage).convert_alpha()
         self.x = 0
         self.y = 0
-    def moveRight(self, right):
-        self.x += right
-    def moveLeft(self, left):
-        self.x += left
-    def moveUp(self, up):
-        self.y += up
-    def moveDown(self, down):
-        self.y += down
+        self.speed = 4
+        self.health = 3
+    def moveRight(self):
+        self.x += self.speed
+    def moveLeft(self):
+        self.x -= self.speed
+    def moveUp(self):
+        self.y += self.speed
+    def moveDown(self):
+        self.y -= self.speed
+    def
 class Ghost:
     def __init__(self, start:tuple):
         self.x = start[0]
         self.y = start[1]
 class Food:
-    def __init__(self, locate:tuple):
+    def __init__(self, locate:tuple, foodImage):
         self.x = locate[0]
         self.y = locate[1]
+        self.radius = 2
         self.color = "orange"
 def main():
     print("#### Testing pacman model ####")

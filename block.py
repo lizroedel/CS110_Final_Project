7c35e8c1a856613f145748f2d68a0a9868138398
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2017-12-07 08:39:05 -0500

    Update block.py

--- block.py
+++ block.py
@@ -1,23 +1,19 @@
+import pygame
+import sys
+import ghost
+import wall
+
 class Block(pygame.sprite.Sprite):
-    # Constructor. Pass in the color of the block,
-    # and its x and y position
     def __init__(self, color, width, height):
-    '''
-    This is the Constructor function __init__
-    param list: (tuple, int, int) self, color, width, height: Pass in the color of the block, and its x and y position
-    return: (tuple, int, int) returns surface of image, color, height, and width
-    '''
-        # Call the parent class (Sprite) constructor
+        '''
+        This is the Constructor function __init__
+        param list: (tuple, int, int) self, color, width, height: Pass in the color of the block, and its x and y position
+        return: (tuple, int, int) returns surface of image, color, height, and width
+        '''
         pygame.sprite.Sprite.__init__(self)
         
-        # Create an image of the block, and fill it with a color.
+        # Creates image and fills with color
         self.image = pygame.Surface([width, height])
-            self.image.fill(self.self.white)
-            self.image.set_colorkey(self.white)
-            pygame.draw.ellipse(self.image,color,[0,0,width,height])
-            
-            # Fetch the rectangle object that has the dimensions of the image
-            # image.
-            # Update the position of this object by setting the values
-            # of rect.x and rect.y
-            self.rect = self.image.get_rect()
+        pygame.draw.ellipse(self.image,color,[0,0,width,height])
+
+        self.rect = self.image.get_rect()
